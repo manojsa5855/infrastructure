7e2fcccf7b62d58828778e09dae893f2aabd2511
manojsa5855 <101385789+manojsa5855@users.noreply.github.com>
2023-05-25 12:21:52 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    storage_account_name = "finalpoc1185"
-    container_name       = "tfstate"
+    storage_account_name = "infrastatefile"
+    container_name       = "infratf"
     key                  = "terraform.tfstate"
     resource_group_name = "Final_POC"
   }
