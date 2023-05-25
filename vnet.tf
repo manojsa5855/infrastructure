e06c37b0fc714ace4b3c30cf7ed19a8384af7920
manojsa5855 <101385789+manojsa5855@users.noreply.github.com>
2023-05-25 15:40:35 +0530

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -1,12 +1,12 @@
 resource "azurerm_virtual_network" "vnet" {
-  name                = "infra_vnet"
+  name                = "infra1_vnet"
   address_space       = ["11.0.0.0/16"]
   location            =var.location
   resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_subnet" "az_subnet" {
-  name                 = "infra_subnet"
+  name                 = "infra1_subnet"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["11.0.0.0/24"]
