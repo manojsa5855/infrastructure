19b33b128ce220776c03a06e95962b81df611311
manojsa5855 <101385789+manojsa5855@users.noreply.github.com>
2023-05-25 15:43:40 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    storage_account_name = "infrastatefile"
-    container_name       = "tfstate"
+    storage_account_name = "infrastatefile1"
+    container_name       = "tfstate1"
     key                  = "terraform.tfstate"
-    resource_group_name = "3-tier"
+    resource_group_name = "3-tier-1"
   }
 }
